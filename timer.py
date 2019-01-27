0239b6facdc8860e03d4a9c7304ccc9daa10d369
Celia <pachecoceliam1@gmail.com>
2019-01-26 17:42:27 -0700

    This is much better

--- timer.py
+++ timer.py
@@ -3,11 +3,10 @@ import time
 from datetime import datetime
 from datetime import timedelta
 import os
+import sys
 
 def getArgs(argv=None):
-    rules = "Remember: \\
-            - No task larger than 5 pomodoros.\\
-            - Any interruption = restart the pomodoro."
+    rules = "Remember:\n- No task larger than 5 pomodoros .\n- Any interruption = restart the pomodoro."
     parser = argparse.ArgumentParser(description="Pomodoro Timer with various \
             length of pomodor", epilog=rules)
     parser.add_argument('-s','--standard', action='store_true', default=True,
@@ -16,61 +15,64 @@ def getArgs(argv=None):
             help='10 min work, 2 min break')
     parser.add_argument('-g','--golden', action='store_true', default=False,
             help='50 min work, 15 min break')
-    parser.add_argument('-bu','--busy', action='store_true', defualt=False,
+    parser.add_argument('-bu','--busy', action='store_true', default=False,
             help='90 min work, 30 min break')
     return parser.parse_args(argv)
 
 
-class Buster:
-    def __init__(self, start_time):
-        self.start = start_time
-        self.stop = 0
-        _TIME = 10
-        _SHORT_BREAK = 2
-        _pomodori = 4
+def pomodori_msg(pomodoro):
+    msg = 'notify-send "BREAK ⏳ Timer done! Take a coffee break! ☕ 🍩 \
+pomodoro {}/{}"'.format(pomodoro + 1, 4)
+    return msg
 
-    def format_time(self):
-        return time.isoformat(timespec="seconds")
-
-    def stop_time(self):
-        self.stop = datetime.time(self.start + timedelta(minutes = TIME)))
-
-    def run(self)
-       print("start {}, end {}".format(self.format_time(self.start), \
-               self.format_time(self.stop))
-       while _pomodori != 0:
-
-            print("{}\r".format(self.format_time(datetime.time(datetime.now()))) \
-                    end="", flush=True)
-
+def break_msg():
+    msg = 'notify-send "WORK ⏳ Timer done! Back to work!"'
+    return msg
 
 def format_times(time):
     return time.isoformat(timespec="seconds")
 
-
-def main():
-    RUNNING = True 
-    TIME = 1 
-    SHORT_BREAK = 5
-    LONG_BREAK = 10
-
-    #start = datetime.datetime.time((datetime.datetime.now()))
+def timer():
+    running = True
     start = datetime.now()
     end = format_times(datetime.time(start + timedelta(minutes = TIME)))
     start = format_times(datetime.time(start))
     print("start {}, end {}".format(start, end))
-
-    while RUNNING:
+    while running:
         now = format_times(datetime.time(datetime.now()))
         print("{}\r".format(now), end="", flush=True)
         if end == now:
-            RUNNING = False
+            running = False 
         else:
-            time.sleep(1)
-    os.system('notify-send "⏳ Timer done! Take a coffee break! ☕ 🍩 :)"')
+            try:
+                time.sleep(1)
+            except KeyboardInterrupt:
+                print("Goodbye")
+                sys.exit(0)
+
+def main(TIME, BREAK):
+    for pomodori in range(0,3):
+        print("Pomodoro {}/{} ".format(pomodori + 1, 4), end="")
+        timer()
+        os.system(pomodori_msg(pomodori))
+        print("Break {} ".format(pomodori + 1), end="")
+        timer()
+        os.system(break_msg())
+
+
 if __name__ == "__main__":
+    #TIME = 25
+    #BREAK= 5
+    TIME = 1
+    BREAK = 1
     args = getArgs()
-    if args 
-
-
-    main()
+    if args.buster:
+        TIME = 10
+        BREAK = 2
+    elif args.golden:
+        TIME = 50
+        BREAK = 15
+    elif args.busy:
+        TIME = 90
+        BREAK = 30
+    main(TIME, BREAK)
